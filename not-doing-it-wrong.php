0405e80c9541ae8b07fa1ef7b8459167b7f314c5
itthinx <itthinx@itthinx.com>
2024-11-26 15:53:53 +0100

    logs data

--- not-doing-it-wrong.php
+++ not-doing-it-wrong.php
@@ -1,7 +1,15 @@
 <?php
 /**
  * Plugin Name: Not Doing it Wrong
- * Description: A WordPress plugin as a last resort to issues when <code>_doing_it_wrong()</code> is too eager and we want to find out what is happening. Avoids triggering a user error for calls to <code>_doing_it_wrong()</code> and gathers information which is logged at shutdown. The plugin file <code>not-doing-it-wrong.php</code> must be placed in <code>mu-plugins</code> so it can catch all instances.
+ * Plugin URI: https://github.com/itthinx/not-doing-it-wrong
+ * Description: A WordPress plugin as a last resort to issues when <code>_doing_it_wrong()</code> is too eager and we want to find out what is happening. Avoids triggering a user error for calls to <code>_doing_it_wrong()</code> and gathers information which is logged at shutdown. The information logged includes originating functions, counts and stack traces. The plugin file <code>not-doing-it-wrong.php</code> must be placed in <code>mu-plugins</code> so it can catch all instances. <strong>The plugin will produce very extensive log entries!</strong>
+ * Version: 1.0.0
+ * Requires at least: 6.0
+ * Requires PHP: 7.4
+ * Author: itthinx
+ * Author URI: https://www.itthinx.com
+ * Donate-Link: https://www.itthinx.com/shop/
+ * License: GPLv3
  */
 
 if ( !defined( 'ABSPATH' ) ) {
@@ -85,7 +93,61 @@ class Not_Doing_It_Wrong {
 	}
 
 	public static function shutdown() {
-		error_log( 'Not Doing it Wrong ... ' . var_export( self::$archive, true ) );
+
+		error_log( '' );
+		error_log( '==========================' );
+		error_log( '= Not Doing it Wrong ... =' );
+		error_log( '==========================' );
+
+		error_log( '' );
+		error_log( '===========' );
+		error_log( '= Summary =' );
+		error_log( '===========' );
+		error_log( '' );
+
+		// Summary
+		$max_function_name = max( array_map( 'strlen', array_keys( self::$archive ) ) );
+		foreach ( self::$archive as $function_name => $data ) {
+			$count = 0;
+			for ( $i = 0; $i < count( $data['messages'] ); $i++ ) {
+				$count += $data['messages'][$i]['count'];
+			}
+			$sep = sprintf( '+-%-'.$max_function_name.'s-+-%-8s-+', str_repeat( '-', $max_function_name ), '--------' );
+			error_log( $sep );
+			error_log( sprintf( '| %-'.$max_function_name.'s | %-8s |', 'Function', 'Count' ) );
+			error_log( $sep );
+			error_log( sprintf( '| %-'.$max_function_name.'s | %8d |', $function_name, $count ) );
+			error_log( $sep );
+		}
+
+		error_log( '' );
+		error_log( '==========' );
+		error_log( '= Detail =' );
+		error_log( '==========' );
+
+		foreach ( self::$archive as $function_name => $data ) {
+
+			error_log( '' );
+			error_log( '* Function : ' . $function_name );
+			error_log( '* Count    : ' . $count );
+			error_log( '' );
+
+			for ( $i = 0; $i < count( $data['messages'] ); $i++ ) {
+				for ( $j = 0; $j < count( $data['messages'][$i]['stack_traces'] ); $j++ ) {
+					$stack_traces = $data['messages'][$i]['stack_traces'];
+					$k = 0;
+					foreach ( $stack_traces as $hash => $stack_trace ) {
+						$k++;
+						error_log( sprintf( '* Stack Trace %d of %d for function %s : ', $k, count( $stack_traces ), $function_name ) );
+						error_log( '' );
+						array_map( 'error_log', explode( "\n", $stack_trace['stack_trace'] ) );
+						error_log( '' );
+					}
+				}
+			}
+		}
+
+		error_log( '' );
 	}
 }
 
