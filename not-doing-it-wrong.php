c0d42fda39800b676cad7d3ee9b8958be57deb60
itthinx <itthinx@itthinx.com>
2024-11-26 13:55:05 +0100

    plugin class

--- not-doing-it-wrong.php
+++ not-doing-it-wrong.php
@@ -1,1 +1,23 @@
 <?php
+/**
+ * Plugin Name: Not Doing it Wrong
+ * Description: A WordPress plugin as a last resort to issues when doing_it_wrong() is too eager and we want to find out what is happening.
+ */
+
+if ( !defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Plugin class.
+ */
+class Not_Doing_It_Wrong {
+
+	/**
+	 * @todo
+	 */
+	public static function init() {
+	}
+}
+
+Not_Doing_It_Wrong::init();
